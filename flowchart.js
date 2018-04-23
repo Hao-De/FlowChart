42329983de8372b3d7d8800513fc2410282ebcf6
GoodGu <patrick2082002@hotmail.com>
2018-04-23 12:29:02 +0800

    add function: remove、clear add shape: arrow、circle

--- flowchart.js
+++ flowchart.js
@@ -26,28 +26,60 @@
 						_canvas = document.querySelector("canvas#"+canvasId);
 						_ctx = _canvas.getContext("2d");
 						_objects = [];
+						
 						if( options )
-							Object.assign( _option, options );
+							_option = Object.assign( {}, _option, options );
 
-						// 如果_option中設定了寬高，變更_canvas之寬高
-						if( _option.width && _option.height ){
-							_canvas.width = _option.width;
-							_canvas.height = _option.height;
-						}else{ // 反之，用_canvas之寬高，記錄到_option中
-							_option.width = _canvas.width;
-							_option.height = _canvas.height;
+						// adjust width and height by _option.width and _option.height
+						_option.width = _option.width || _canvas.offsetWidth;
+						_option.height = _option.height || _canvas.offsetHeight;
+						_canvas.setAttribute( 'width', _option.width );
+						_canvas.setAttribute( 'height', _option.height );
+
+						// add css text inside the head tag
+						let style = document.createElement("style");
+						let css = `canvas#${canvasId}{width:${_option.width}px;height:${_option.height}px;}`;
+						style.type = 'text/css';
+						if (style.styleSheet){
+							style.styleSheet.cssText = css;
+						} else {
+							style.appendChild(document.createTextNode(css));
 						}
-
+						document.head.appendChild(style);
+						
+						// make canvas high resolution
+						if( window.devicePixelRatio ){
+							let ratio = window.devicePixelRatio;
+							_canvas.setAttribute( 'width', _option.width * ratio );
+							_canvas.setAttribute( 'height', _option.height * ratio );
+							_canvas.style.width = _option.width;
+							_canvas.style.height = _option.height;
+							_ctx.scale( ratio, ratio );
+						}
+						
 						// 校正 html canvas 線條模糊( 所有座標平移 0.5px，且線條盡量以整數座標繪製，所以大部分之座標皆以 Math.round 取整數 )
 						_ctx.translate(0.5, 0.5);
 						
 						this.repaint( _ctx, _objects, _option );
-
 					}catch( error ){
-						console.error("[FlowChart] ", error.message);
+						console.error("[FlowChart] ", error);
 					}
 				}
 
+				/**
+				 * create a circle data
+				 * @param {object} options - options of the rectangle
+				 */
+				static circle( options ){
+					// default setting of rectangle object
+					let setting = shape( "CIRCLE" );
+					let extendSetting = {
+						radius: 0.5,
+					}
+					setting = Object.assign( {}, setting, options );
+					return setting;
+				}
+				
 				/**
 				 * create a rectangle data
 				 * @param {object} options - options of the rectangle
@@ -55,7 +87,7 @@
 				static rect( options ){
 					// default setting of rectangle object
 					let setting = shape( "RECTANGLE" );
-					Object.assign( setting, options );
+					setting = Object.assign( {}, setting, options );
 					return setting;
 				}
 
@@ -66,7 +98,7 @@
 				static rhom( options ){
 					// default setting of rhombus object
 					let setting = shape( "RHOMBUS" );
-					Object.assign( setting, options );
+					setting = Object.assign( {}, setting, options );
 					return setting;
 				}
 				
@@ -85,7 +117,7 @@
 						alignX: "left",
 						alignY: "top"
 					};
-					Object.assign( setting, options );
+					setting = Object.assign( {}, setting, options );
 					return setting;
 				}
 				
@@ -94,20 +126,31 @@
 				 * @param {object} options - options of the rhombus
 				 */
 				static arrow( options ){
-					// default setting of rhombus object
+					// default setting of arrow object
 					let setting = {
 						type: "ARROW",
-						lineWidth: 0.01,
+						lineWidth: 0.05,
 						arrowhead: true,
+						twoHead: false,
 						point: [],
-						color: "black"
+						color: "black",
 					};
-					Object.assign( setting, options );
+					// default setting of headstyle
+					let defaultHeadStyle = {
+						headlen: 0.2,
+						lineWidth: 0.05,
+						color: "black",
+						theta: 30,
+					}
+					
+					setting = Object.assign( {}, setting, options );
+					setting.headStyle = Object.assign( {}, defaultHeadStyle, options.headStyle );
+					
 					return setting;
 				}
 
 				/**
-				 * add an object or objects into array and print it on the canvas
+				 * add an object or objects into array and paint it on the canvas
 				 * @param {array} newItems - an array that stores objects added to the canvas
 				 */
 				add( ...newItems ){
@@ -115,7 +158,26 @@
 					if( _option.repaintAfterModify )
 						this.repaint( _ctx, _objects, _option );
 				}
-
+				
+				/**
+				 * remove an object or objects from the _objects array and repaint.
+				 * @param {array} removeObj - an array that stores objects needing to be removed from the canvas.
+				 */
+				remove( ...removeObj ){
+					removeObj.forEach(( obj )=>{
+						let removeIndex = _objects.indexOf( obj );
+						if( removeIndex >= 0 )
+							_objects.splice( removeIndex, 1 );
+					});
+					if( _option.repaintAfterModify )
+						this.repaint( _ctx, _objects, _option );
+				}
+				
+				clear(){
+					_objects = [];
+					this.repaint( _ctx, _objects, _option );
+				}
+				
 				/**
 				 * set gap width
 				 * @param {integer} value - the width of gap (pixel).
@@ -147,6 +209,9 @@
 					if( objects.length ){
 						objects.forEach( function( value ){
 							switch( value.type ){
+								case "CIRCLE":
+									paintCircle( ctx, value, globalOption );
+									break;
 								case "RECTANGLE":
 									if( !value.radius )
 										paintRect( ctx, value, globalOption );
@@ -189,7 +254,7 @@
 					isFill: false,
 					fillColor: "gray",
 					strokeColor: "#000",
-					lineWidth: 1
+					lineWidth: 0.05
 				}
 			}
 			
@@ -199,10 +264,11 @@
 			 * @param {object} globalOption - options of canvas
 			 */
 			function paintGrid( ctx, globalOption ){
-				if(globalOption.hasGrid){
+				if( globalOption.hasGrid ){
 					let maxWidth = globalOption.width;
 					let maxHeight = globalOption.height;
 
+					ctx.save();
 					ctx.beginPath();
 					ctx.strokeStyle = "#D3D3D3";
 					ctx.lineWidth = 0.05;
@@ -216,9 +282,33 @@
 						ctx.lineTo( Math.round(maxWidth), Math.round(y) );
 						ctx.stroke();
 					}
+					ctx.restore();
 				}
 			}
 
+			/**
+			 * paint single circle
+			 * @param {CanvasRenderingContext2D} ctx - the context of canvas
+			 * @param {object} option - option of the circle
+			 * @param {object} globalOption - options of canvas
+			 */
+			function paintCircle( ctx, option, globalOption ){
+				ctx.save();
+				ctx.beginPath();
+				ctx.strokeStyle = option.strokeColor;
+				ctx.lineWidth = globalOption.gap * option.lineWidth;
+				ctx.arc( Math.round( globalOption.gap * option.x ), 
+						 Math.round( globalOption.gap * option.y ), 
+						 globalOption.gap * option.radius, 
+						 0, 2 * Math.PI );
+				ctx.stroke();
+				if( option.isFill ){
+					ctx.fillStyle = option.fillColor;
+					ctx.fill();
+				}
+				ctx.restore();
+			}
+			
 			/**
 			 * paint single rectangle
 			 * @param {CanvasRenderingContext2D} ctx - the context of canvas
@@ -226,9 +316,10 @@
 			 * @param {object} globalOption - options of canvas
 			 */
 			function paintRect( ctx, option, globalOption ){
+				ctx.save();
 				ctx.beginPath();
 				ctx.strokeStyle = option.strokeColor;
-				ctx.lineWidth = option.lineWidth;
+				ctx.lineWidth = globalOption.gap * option.lineWidth;
 				ctx.rect( Math.round( globalOption.gap * option.x ), 
 						 Math.round( globalOption.gap * option.y ), 
 						 Math.round( globalOption.gap * option.width ), 
@@ -238,6 +329,7 @@
 					ctx.fillStyle = option.fillColor;
 					ctx.fill();
 				}
+				ctx.restore();
 			}
 
 			/**
@@ -264,6 +356,7 @@
 						option.radius[side] = option.radius[side] || defaultRadius[side];
 					}
 				}
+				ctx.save();
 				ctx.beginPath();
 				ctx.strokeStyle = option.strokeColor;
 				ctx.lineWidth = option.lineWidth;
@@ -282,6 +375,7 @@
 					ctx.fillStyle = option.fillColor;
 					ctx.fill();
 				}
+				ctx.restore();
 			}
 			
 			/**
@@ -296,6 +390,7 @@
 				let width = Math.round( globalOption.gap * option.width );
 				let height = Math.round( globalOption.gap * option.height );
 
+				ctx.save();
 				ctx.beginPath();
 				ctx.strokeStyle = option.strokeColor;
 				ctx.lineWidth = option.lineWidth;
@@ -310,6 +405,7 @@
 					ctx.fillStyle = option.fillColor;
 					ctx.fill();
 				}
+				ctx.restore();
 			}
 			
 			/**
@@ -321,29 +417,23 @@
 			function paintText( ctx, option, globalOption ){
 				
 				if( option.text ){
-					
+					ctx.save();
 					// set font size and font family
 					if( typeof option.fontSize !== "number" )
 						option.fontSize = parseFloat( option.fontSize );
 					ctx.font = ( option.fontSize * globalOption.gap ) + "px " + option.fontFamily;
 					
-					// set the width of text
-					let width = ctx.measureText(option.text).width;
-					if( typeof option.maxWidth === "number" && ( option.maxWidth * globalOption.gap ) < width )
-						width = option.maxWidth * globalOption.gap;
-					
-					// accroding to the align point, calculate the left bottom coordinate
-					let x, y;
+					// align the text
 					switch( option.alignX ){
 						case "center":
-							x = Math.round( globalOption.gap * option.x - width/2 );
+							ctx.textAlign = "center";
 							break;
 						case "right":
-							x = Math.round( globalOption.gap * option.x - width );
+							ctx.textAlign = "right";
 							break;
 						case "left":
 						default:
-							x = Math.round( globalOption.gap * option.x );
+							ctx.textAlign = "left";
 							break;
 					}
 					switch( option.alignY ){
@@ -358,13 +448,20 @@
 							ctx.textBaseline = "top";
 							break;
 					}
-					y = Math.round( globalOption.gap * option.y );
+					let x = Math.round( globalOption.gap * option.x );
+					let y = Math.round( globalOption.gap * option.y );
 					
 					ctx.fillStyle = option.color;
-					if( typeof option.maxWidth === "number" )
-						ctx.fillText( option.text, x, y, width );
+					
+					// measure text width
+					let width = ctx.measureText(option.text).width;
+					
+					// if custom width < measure width, limit it while drawing
+					if( typeof option.maxWidth === "number" && ( option.maxWidth * globalOption.gap ) < width )
+						ctx.fillText( option.text, x, y, option.maxWidth * globalOption.gap );
 					else
 						ctx.fillText( option.text, x, y );
+					ctx.restore();
 				}
 			}
 			
@@ -375,9 +472,106 @@
 			 * @param {object} globalOption - options of canvas
 			 */
 			function paintArrow( ctx, option, globalOption ){
+				
 				if( Array.isArray( option.point ) && option.point.length > 0 ){
 					
+					ctx.save();
+					ctx.beginPath();
+					ctx.strokeStyle = option.color;
+					ctx.lineWidth = option.lineWidth * globalOption.gap;
+					
+					// paint every line
+					option.point.forEach(( value, index, array )=>{
+						if( index === 0 ){
+							ctx.moveTo( value[0] * globalOption.gap, value[1] * globalOption.gap );
+						}else{
+							ctx.lineTo( value[0] * globalOption.gap, value[1] * globalOption.gap );
+						}
+					});
+					ctx.stroke();
+					ctx.restore();
+					
+					// draw arrowhead if needed.
+					if( option.arrowhead ){
+						let lastTwoPoint = option.point.slice(-2);
+						paintArrowHead(
+							ctx, {
+								fromX: lastTwoPoint[0][0]*globalOption.gap,
+								fromY: lastTwoPoint[0][1]*globalOption.gap,
+								toX: lastTwoPoint[1][0]*globalOption.gap,
+								toY: lastTwoPoint[1][1]*globalOption.gap,
+								theta: option.headStyle.theta,
+								headlen: option.headStyle.headlen*globalOption.gap,
+								width: option.headStyle.lineWidth*globalOption.gap,
+								color: option.headStyle.color
+							}, globalOption
+						);
+						
+						// draw reversely arrowhead, if needed.
+						if( option.twoHead ){
+							let firstTwoPoint = option.point.slice(0, 2);
+							paintArrowHead(
+								ctx, {
+									fromX: firstTwoPoint[1][0]*globalOption.gap,
+									fromY: firstTwoPoint[1][1]*globalOption.gap,
+									toX: firstTwoPoint[0][0]*globalOption.gap,
+									toY: firstTwoPoint[0][1]*globalOption.gap,
+									theta: option.headStyle.theta,
+									headlen: option.headStyle.headlen*globalOption.gap,
+									width: option.headStyle.lineWidth*globalOption.gap,
+									color: option.headStyle.color
+								}, globalOption
+							);
+						}
+					}
+					
 				}
+			}
+			
+			/**
+			 * 參考來源: https://www.w3cplus.com/canvas/drawing-arrow.html © w3cplus.com
+			 * paint arrow head
+			 * @param {CanvasRenderingContext2D} ctx - the context of canvas
+			 * @param {object} points - point of the arrow
+			 * @param {object} style - style of the arrow
+			 * @param {object} globalOption - options of canvas
+			 */
+			function paintArrowHead(ctx, option, globalOption) {
+				let setting = {
+					fromX: 0,
+					fromY: 0,
+					toX: 0,
+					toY: 0,
+					theta: 30,
+					headlen: 0.3 * globalOption.gap,
+					width: 0.1 * globalOption.gap,
+					color: "#000",
+				};
+				setting = Object.assign( {}, setting, option );
+				
+				let angle = Math.atan2(setting.fromY - setting.toY, setting.fromX - setting.toX) * 180 / Math.PI;
+				let angle1 = (angle + setting.theta) * Math.PI / 180;
+				let angle2 = (angle - setting.theta) * Math.PI / 180;
+				let topX = setting.headlen * Math.cos(angle1);
+				let topY = setting.headlen * Math.sin(angle1);
+				let botX = setting.headlen * Math.cos(angle2);
+				let botY = setting.headlen * Math.sin(angle2);
+				
+				ctx.save();
+				ctx.beginPath();
+				
+				// Reverse length on the other side 
+				let arrowX = setting.toX + topX;
+				let arrowY = setting.toY + topY;
+				ctx.moveTo(arrowX, arrowY);
+				ctx.lineTo(setting.toX, setting.toY);
+				arrowX = setting.toX + botX;
+				arrowY = setting.toY + botY;
+				ctx.lineTo(arrowX, arrowY);
+				ctx.strokeStyle = setting.color;
+				ctx.lineWidth = setting.width;
+				ctx.stroke();
+				ctx.restore();
 			}
 		})();
 	}
